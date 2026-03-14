3722f63da8a1e8dcc7bbe8aead496a2562014cc7
FooZiken <FooZiken@gmail.com>
2026-03-15 00:39:43 +0300

    users module completed (dto validation + bcrypt); auth module scaffold added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 
 import { UsersModule } from './modules/users/users.module';
 import { User } from './modules/users/entities/user.entity';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -25,6 +26,8 @@ import { User } from './modules/users/entities/user.entity';
     }),
 
     UsersModule,
+
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/modules/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll() {
+    return this.usersRepository.find();
+  }
+
+  async create(userData: Partial<User>) {
+    if (!userData.password) {
+      throw new Error('Password is required');
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const user = this.usersRepository.create({
+      email: userData.email,
+      password: hashedPassword,
+      name: userData.name,
+      roles: userData.roles,
+    });
+
+    return this.usersRepository.save(user);
+  }
+}
